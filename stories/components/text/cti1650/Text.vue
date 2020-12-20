798a4d664491e6d0fcd3069976ba77abd5834f0f
cti1650 <cti1650@gmail.com>
2020-12-20 22:32:58 +0900

    初めてgitを触るため動作確認させて頂いています！

---
+++ stories/components/text/cti1650/Text.vue
@@ -0,0 +1,40 @@
+<template>
+  <p :class="classes" :style="style">{{ label }}</p>
+</template>
+
+<script>
+import './text.css';
+
+export default {
+  name: 'notice-text',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: false,
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+
+  computed: {
+    classes() {
+      return {
+        'storybook-text': true,
+        'storybook-text--question': this.type === 'question',
+        'storybook-text--danger': this.type === 'danger',
+      };
+    },
+    style() {
+      return {
+        backgroundColor: this.backgroundColor,
+      };
+    },
+  },
+};
+</script>
