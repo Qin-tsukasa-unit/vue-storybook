ac0dcd984ab6152f558ad9ff3169b65b5bbc7e72
thayashi1988 <tatsurouhayashi0210@gmail.com>
2020-12-22 21:37:10 +0900

    :rocket:セレクトコンポーネント追加

---
+++ stories/components/select/thayashi1988/SelectOption.vue
@@ -0,0 +1,16 @@
+<template>
+  <option
+    :value="optionValue.valueData"
+  >{{ optionValue.txtData }}</option>
+</template>
+
+<script>
+import './select.css';
+
+export default {
+  name: 'SelectOption',
+  props: {
+    optionValue: Object,
+  },
+};
+</script>
