11c6e7557975899521ba5bcc040739b2703bb899
kondo tomonori <tomonorik1001@gmail.com>
2020-12-24 23:16:53 +0900

    ボタン風チェックボックスの作成

---
+++ stories/components/checkbox/kon/Checkbox.stories.js
@@ -0,0 +1,28 @@
+import MyCheckbox from './Checkbox.vue';
+
+export default {
+  title: 'Example/Checkbox/kon',
+  component: MyCheckbox,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    color:{ control: 'color' },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { MyCheckbox },
+  template: '<my-checkbox @onClick="onClick" v-bind="$props" />',
+});
+
+export const Checkbox = Template.bind({});
+Checkbox.args = {
+  label: 'Checkbox',
+};
+
+export const Easing = Template.bind({});
+Easing.args = {
+  easing:true,
+  label: 'Easing',
+};
+
