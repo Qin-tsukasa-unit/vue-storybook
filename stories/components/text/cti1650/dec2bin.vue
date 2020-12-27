9de0ba8a2752f2a4ab9c1d576fa6d9223326f31b
cti1650 <cti1650@gmail.com>
2020-12-27 12:35:03 +0900

    初めてコミットしました！VScode自体使い慣れていないですが練習で作成してみましたのでご確認下さい。

---
+++ stories/components/text/cti1650/dec2bin.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input v-if="!hidden" type="number" v-model="numDec"> <b>{{ toBin }}</b>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'dec2bin',
+
+    props:{
+        numDec: { type: Number, default: 0 },
+        hidden: { type: Boolean, default: false },
+    },
+    computed: {
+        toBin: function(){
+            return parseInt(parseInt(this.numDec).toString(2));
+        },
+    },
+};
+</script>
+
+<style scoped>
+  div,input{
+    font-size:18px;
+  }
+</style>
