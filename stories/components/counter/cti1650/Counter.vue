33ef9c22e4396347db73b3173bdb0e5b4751d5b2
cti1650 <cti1650@gmail.com>
2021-01-03 16:25:32 +0900

    例題でよくあるカウンターを複数追加して個別にカウントできるようにしました。

---
+++ stories/components/counter/cti1650/Counter.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="counter">
+        <a href="#" class="btn-sticky btn" @click.prevent="addCounter" style="font-size:20px;"><b class="l"> {{this.title}} </b><b class="r">{{ this.count }}</b></a>
+    </div>
+</template>
+ 
+<script>
+    export default {
+        name: 'counter',
+
+        props:{
+            title: { type: String, default: 'counter' },
+            step: { type: Number, default: 1 },
+            defCount: { type: Number, default: 0 }
+        },
+        data: function(){
+            return {
+                count: this.defCount
+            }
+        } ,
+        methods:{
+            addCounter(){
+                this.count += this.step;
+                this.$emit('add',this.count);
+            }
+        } ,
+    };
+</script>
+
+<style scoped>
+    .btn-sticky {
+        display: inline-block;
+        padding: 0.5em 1em;
+        text-decoration: none;
+        background: #f7f7f7;
+        border-left: solid 6px #ff7c5c;/*左線*/
+        color: #ff7c5c;/*文字色*/
+        font-weight: bold;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
+    }
+    .btn-sticky:active {
+        box-shadow: inset 0 0 2px rgba(128, 128, 128, 0.1);
+        transform: translateY(2px);
+    }
+    .btn{
+        height:50px;
+        width:100%;
+        max-width:300px;
+        position: relative;
+    }
+    div.counters{
+        width:100%;
+        max-width:300px;
+    }
+    *{
+        touch-action: manipulation;
+        user-select: none; /* CSS3 */
+        -moz-user-select: none; /* Firefox */
+        -webkit-user-select: none; /* Safari、Chromeなど */
+        -ms-user-select: none; /* IE10かららしい */
+    }
+    .btn b.l{
+        position: absolute;
+        left: 15px;
+    }
+    .btn b.r{
+        position: absolute;
+        right: 15px;
+        font-size:30px;
+    }
+</style>
---
+++ stories/components/counter/cti1650/Counters.stories.js
@@ -0,0 +1,16 @@
+import counters from './Counters.vue';
+
+export default {
+  title: 'Example/counter/cti1650',
+  component: counters,
+  argTypes: {},
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { counters },
+  template: '<counters />',
+});
+
+export const Sample = Template.bind({});
+Sample.args = {};
