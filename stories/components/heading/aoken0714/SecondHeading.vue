abd370c5e8d185f3f74e444b03d438b5caf06258
k.aoki <k.aoki0714@gmail.com>
2021-01-12 05:37:27 +0900

    Headingを「render」で実装

---
+++ stories/components/heading/aoken0714/SecondHeading.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  render: function (createElement) {
+    return createElement("h" + this.level, this.$slots.default);
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true,
+      validator: function (value) {
+        return [1, 2, 3].includes(value);
+      },
+    },
+  },
+};
+</script>
