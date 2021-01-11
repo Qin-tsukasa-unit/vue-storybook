7e164d4746f3d70ce4573078129cb0f343e74170
k.aoki <k.aoki0714@gmail.com>
2021-01-11 15:45:30 +0900

    List.vue 実装

--- stories/components/List/List.vue
+++ stories/components/List/List.vue
@@ -1,13 +1,43 @@
 <template>
-  
+  <ol v-if="hasOrderedList" :type="orderType">
+    <li v-for="item in itemList" :key="item">
+      {{ item }}
+    </li>
+  </ol>
+  <ul v-else :type="unOrderType">
+    <li v-for="item in itemList" :key="item">
+      {{ item }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-  
+  props: {
+    hasOrderedList: {
+      type:Boolean,
+      default: false,
+    },
+    orderType: {
+      type: String,
+      default: "1",
+      validator: function (value) {
+        return ['1', 'a', 'i'].filter( type => type === value).length > 0;
+      },
+    },
+    unOrderType: {
+      type: String,
+      default: "disc",
+      validator: function (value) {
+        return ['disc', 'circle', 'square'].filter(type => type === value).length > 0;
+      },
+    },
+    itemList: {
+      type: Array,
+      default: ["a", "b", "c"],
+    },
+  },
+  computed: {
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
